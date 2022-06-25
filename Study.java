c6af8fd809987a32589245182d4516aece2fc851
Tzu-Chao Wang <john29917958@gmail.com>
2022-06-25 08:49:29 +0800

    Add string usage example

--- Study.java
+++ Study.java
@@ -1,12 +1,21 @@
 public class Study {
     public static void main(String[] args) {
         System.out.println("=========== Primitive types ===========");
-        System.out.println("----------- Integer -----------");
+        System.out.println("[ Integer ]");
         Integer integer = 10;
         Class classType = integer.getClass();
         System.out.println(String.format("Value of \"integer\" is: %s", integer));
         System.out.println(String.format("Type of \"integer\" is: %s", classType.getName()));
 
+        int intP = 10;
+        System.out.println(String.format("Value of \"integer\" is: %s", intP));
 
-    }    
+        System.out.println("[ String ]");
+        String str = "Hello, Kimmy";
+        classType = str.getClass();
+        System.out.println(String.format("Value of \"str\" is: %s", str));
+        System.out.println(String.format("Type of \"str\" is: %s", classType.getName()));
+
+
+    }
 }
