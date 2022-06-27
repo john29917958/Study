231a8cdb4751f336631ddb9a4fc07a76a20c5db0
Tzu-Chao Wang <john29917958@gmail.com>
2022-06-28 06:28:25 +0800

    Add array and map to Study.java

--- Study.java
+++ Study.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import static java.util.Map.entry;
+
 public class Study {
     public static void main(String[] args) {
         System.out.println("=========== Primitive types ===========");
@@ -8,7 +13,6 @@ public class Study {
         int intP = 10;
         System.out.println(String.format("Value of \"integer\" declared with primitive keyword is: %s", intP));
         System.out.println(String.format("Type of \"integer\" is: %s", classType.getName()));
-
 
         System.out.println("[ String ]");
         String str = "Hello, Kimmy";
@@ -26,6 +30,22 @@ public class Study {
 
         double precisedNumber = 0.1;
         System.out.println(String.format("Value of \"precisedNumber\" is: %s", precisedNumber));
-        //System.out.println(String.format("Type of \"precisedNumber\" is: %s", classType.getName()));
+        
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+        classType = arr.getClass();
+        System.out.println("Values \"arr\" is: " + Arrays.toString(arr));
+        System.out.println("Type of \"arr\" is: " + classType.getName());
+
+        Map<String, String> map = new HashMap<String, String>(Map.ofEntries(
+            entry("title1", "Java"),
+            entry("title2", "is")
+        ));
+        map.put("title3", "awesome!");
+        classType = map.getClass();
+        System.out.println("Value of map is: ");
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+        System.out.println(String.format("Type of \"map\" is: %s", classType.getName()));
     }
 }
