49575b7e7fa9d9de8ebc4d14046d6e854b3bac58
Tzu-Chao Wang <john29917958@gmail.com>
2022-07-02 20:13:01 +0800

    Create two threads to run server and task logic simultaneously

--- Client-Server-Model/p-client.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get("http://localhost:8080/", params={})
-data = response.json()
-
-print(data)
--- Client-Server-Model/p-server.py
+++ Client-Server-Model/p-server.py
@@ -1,10 +1,13 @@
+from concurrent.futures import thread
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+import time
+import requests
 
 hostName = "localhost"
 serverPort = 8080
 
-
-class PServer(BaseHTTPRequestHandler):
+class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
@@ -13,15 +16,29 @@ class PServer(BaseHTTPRequestHandler):
             "response": "Res"
         }""", "utf-8"))
 
-
-if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), PServer)
+def runServer():
+    webServer = HTTPServer((hostName, serverPort), Server)
     print(f"Server started http://{hostName}:{serverPort}")
-
+    
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
 
     webServer.server_close()
-    print("Server stopped.")
+    print("Server stopped.")        
+
+def runTask():
+    print("Task started")
+    while True:
+        time.sleep(1)
+        response = requests.get("http://localhost:8080")
+        data = response.json()
+        print(f"Name: {data['name']}, response: {data['response']}")
+
+if __name__ == "__main__":    
+    serverThread = threading.Thread(target=runServer, daemon=False)
+    taskThread = threading.Thread(target=runTask, daemon=True)
+    serverThread.start()
+    taskThread.start()
+    
