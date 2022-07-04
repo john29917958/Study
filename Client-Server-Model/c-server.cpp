b2a085124a4cff082775fe566f95d862572ef15c
Tzu-Chao Wang <john29917958@gmail.com>
2022-07-04 09:17:24 +0800

    Implement C++ socket server

--- Client-Server-Model/c-server.cpp
+++ Client-Server-Model/c-server.cpp
@@ -1,20 +1,89 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <thread>
 
+struct sockaddr_in;
 bool is_ended = false;
 
-void run() {
-    while (!is_ended) {
-        std::cout << "Info: Server loop is executed once." << std::endl;
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+void run_server(int port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        error("Error opening socket");
+    }
+
+    sockaddr_in serv_addr;
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        error("Error on binding");
+    }
+
+    listen(sockfd, 5);
+    sockaddr_in cli_addr;
+    socklen_t clilen = sizeof(cli_addr);
+
+    int newsockfd;
+
+    while (!is_ended)
+    {
+        int newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0)
+        {
+            error("Error on accept");
+        }
+
+        send(newsockfd, "Hello, world!\n", 13, 0);
+        char buffer[256];
+        bzero(buffer, 256);
+        int n = read(newsockfd, buffer, 255);
+        if (n < 0)
+        {
+            error("ERROR reading from socket");
+        }
+
+        std::cout << "got connection from " << inet_ntoa(cli_addr.sin_addr)
+            << " at port " << ntohs(cli_addr.sin_port)
+            << ": " << buffer << std::endl;
+        std::cout << "Press any key to leave C++ socket server..." << std::endl;
+    }
+
+    if (newsockfd > 0)
+    {
+        close(newsockfd);
+    }
+
+    if (sockfd > 0)
+    {
+        close(sockfd);
     }
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    std::thread t(run);
+    int port = atoi(argv[1]);
 
-    char input;
-    scanf("%c", &input);
+    std::string input;
+    std::thread server_t(run_server, port);
+    std::cout << "Press any key to leave C++ socket server..." << std::endl;
+    std::getline(std::cin, input);
+
     is_ended = true;
 
     return 0;
